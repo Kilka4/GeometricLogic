dc07bd753fbedd00acb28cc3fd3847c97e6e631e
Kilka4 <135958639+Kilka4@users.noreply.github.com>
2023-08-01 17:24:05 +0300

    ReachingTheNextLevel

--- Scripts/ReachingTheNextLevel.gd
+++ Scripts/ReachingTheNextLevel.gd
@@ -5,9 +5,20 @@ signal LevelUnlocked
 var triangle_entered = false
 var square_entered = false
 var circle_entered = false
+var name_body = ["Square", "Circle", "Triangle"]
+var body_entered = []
 
 
 func _on_Trigger_body_entered(body):
-	if triangle_entered == true && square_entered == true && circle_entered == true:
-		emit_signal("LevelUnlocked")
-		print("Next Level")
+	print("body", body_entered)
+	if name_body.has(body.name) && !body_entered.has(body):
+		body_entered += [body]
+		print("body_entered", body_entered)
+		if body_entered.size() == 3:
+			emit_signal("LevelUnlocked")
+			print("Next Level")
+
+
+func _on_Trigger_body_exited(body):
+	if body_entered.has(body):
+		body_entered.erase(body)
