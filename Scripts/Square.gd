be47d7d853fbd637bcdce12a6908e60dd63ed697
Kilka4 <135958639+Kilka4@users.noreply.github.com>
2023-09-14 12:15:59 +0300

    created the effect of reducing the square, added a shader to all scenes

--- Scripts/Square.gd
+++ Scripts/Square.gd
@@ -8,6 +8,8 @@ var max_slides: int = 4
 var floor_max_angle: float = 0.785398
 var mass: float = 1
 var inertia = 3
+var can_decrease = true
+var in_decrease = false
 
 var velocity = Vector2()
 var jumping = false
@@ -17,36 +19,46 @@ func get_input():
 	var right = Input.is_action_pressed("Player1d")
 	var left = Input.is_action_pressed("Player1a")
 	var jump = Input.is_action_just_pressed("Player1jump")
-	var right_rotation = Input.is_action_just_pressed("Player1Rightrotation")
-	var left_rotation = Input.is_action_just_pressed("Player1Leftrotation")
+	var decrease = Input.is_action_just_pressed("Decrease1player")
 	
-	if jump && is_on_floor():
+	
+	if jump && is_on_floor() && in_decrease == false:
 		jumping = true
 		velocity.y = jump_speed
-		$AnimationPlayer.play("jump")
+		$AnimationPlayer.play("Normal Jump")
+	if jump && is_on_floor() && in_decrease == true:
+		jumping = true
+		velocity.y = jump_speed
+		$AnimationPlayer.play("Jump with Decrease")
+		
+		
 	if right:
 		velocity.x += speed
 		$Sprite.flip_h = false
 	if left:
 		velocity.x -= speed
 		$Sprite.flip_h = true
-	if !is_on_floor() && right_rotation:
-		$Sprite.rotation_degrees += 45
-		$CollisionShape2D.rotation_degrees += 45
-	if !is_on_floor() && left_rotation:
-		$Sprite.rotation_degrees -= 45
-		$CollisionShape2D.rotation_degrees -= 45
+	if decrease && can_decrease == true:
+		if in_decrease == false:
+			$AnimationPlayer.play("decrease")
+			in_decrease = true
+		else:
+			$AnimationPlayer.play("increase")
+			in_decrease = false
 
 
 func _on_Transfer_body_entered(body):
 	if body.is_in_group("Circle") || body.is_in_group("Triangle"):
 		body.gravity = 1
 		body.position.x = position.x
+	if body.name == "TilesetWhite" && in_decrease == true:
+		can_decrease = false
 
 
 func _on_Transfer_body_exited(body):
 	if body.is_in_group("Circle") || body.is_in_group("Triangle"):
 		body.gravity = 1200
+	can_decrease = true
 
 func limit_pos_to_camera():
 	var _camera_rect = Global.current_camera2d.global_rect as Rect2
